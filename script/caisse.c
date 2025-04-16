11f15a635c6b4a8af1df5490c9d52f6cd746339d
Elisassh <elisatony021@gmail.com>
2025-04-16 21:10:36 +0400

    Tag v0.2

--- script/caisse.c
+++ script/caisse.c
@@ -3,6 +3,9 @@
 #include <time.h>
 
 
+double calcule_monnaie(double valeur_demandee, double valeur_donnee);
+
+
 int main(){
 
 	srand(time(0));
@@ -21,5 +24,13 @@ int main(){
 	}while(valeur_demandee>valeur_donnee);
 
 	printf("Vous avez paye un montant de Rs : %.2f\n", valeur_donnee);
+	calcule_monnaie(valeur_demandee, valeur_donnee);
 
+}
+
+double calcule_monnaie(double valeur_demandee, double valeur_donnee){
+
+  double monnaie_a_rendre = valeur_donnee - valeur_demandee;
+  printf("Votre monnaie est : Rs %.2f\n", monnaie_a_rendre);
+  
 }
